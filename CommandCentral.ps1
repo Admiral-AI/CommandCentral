1879c026773497eaf87d23c084a9263196920d05
Admiral-AI <ymaronowitz@gmail.com>
2024-01-01 22:08:34 -0500

    Added credential caching and retrieval

--- CommandCentral.ps1
+++ CommandCentral.ps1
@@ -1,32 +1,98 @@
 # Main function
 function Main {
-    # Define the directory where PowerShell scripts are located
-    $functionDirectory = "Functions"
-    # Get the root directory of the script
-    $startingDirectory = $PSScriptRoot
-    # Combine the root directory with the script directory
-    $startingDirectory = Join-Path $startingDirectory $functionDirectory
 
     # Clear the console
     Clear-Host
 
-    # Call the Set-DisplayMenu function with the starting directory
+    Get-UserCredentials
+
+}
+
+function Get-UserCredentials {
+    <#
+    param (
+        [string]$startingDirectory
+    )
+    #>
+    
+    # Get the CIM_ComputerSystem CIM class
+    $computerSystem = Get-CimInstance Win32_ComputerSystem
+    # Check if the domain property is not empty
+    if ($computerSystem.PartofDomain -eq $true) {
+        Write-Host "Computer is in a domain: $($computerSystem.Domain)"
+        
+        # Will further expand on this section to query AD to try to find all admin account associated with the current user
+        # But for the time being I will statically program 3 credential sets: Regular, Sprt, ADM
+
+        # Try to get stored credentials, and if any of them are null, prompt for credentials
+        $regUserCred = Get-StoredCredential -Target CommandCentral-User -AsCredentialObject
+        $sprtUserCred = Get-StoredCredential -Target CommandCentral-Sprt -AsCredentialObject
+        $admUserCred = Get-StoredCredential -Target CommandCentral-ADM -AsCredentialObject
+
+        # Check if any of the stored credentials are null, and prompt for credentials if needed
+        if ($null -eq $regUserCred) {
+            $regUserCred = Get-Credential -UserName $env:USERNAME -Message "Please provide your credentials for the following account: $($env:USERNAME)"
+            $regUserCred = New-StoredCredential -Target CommandCentral-User -Credential $regUserCred -Persist ENTERPRISE
+        }
+
+        if ($null -eq $sprtUserCred) {
+            $sprtUsername = "$($env:USERNAME)_sprt"
+            $sprtUserCred = Get-Credential -UserName $sprtUsername -Message "Please provide your credentials for the following account: $($env:USERNAME)_sprt"
+            $sprtUserCred = New-StoredCredential -Target CommandCentral-Sprt -Credential $sprtUserCred -Persist ENTERPRISE
+        }
+
+        if ($null -eq $admUserCred) {
+            $admUsername = "$($env:USERNAME)_adm"
+            $admUserCred = Get-Credential -UserName $admUsername -Message "Please provide your credentials for the following account: $($env:USERNAME)_adm"
+            $admUserCred = New-StoredCredential -Target CommandCentral-ADM -Credential $admUserCred -Persist ENTERPRISE
+        }
+
+    } else {
+        Write-Host "Computer is in a workgroup, credentials are assumed to not be needed (administrator running script)"
+    }
+
+    # Clear the screen before heading to the next functions
+    Clear-Host
+
+    # Call the Set-DisplayMenu function and pass the starting directory from Main
     Set-DisplayMenu -startingDirectory $startingDirectory
+
 }
 
 # Function to display a menu and handle user input
 function Set-DisplayMenu {
-    # Parameters from Main function
+    <# Parameters from Main function
     param (
         [string]$startingDirectory
     )
+    #>
+
+    # Define the directory where the PowerShell scripts and 'menus' are located
+    $functionDirectory = "Functions"
+    # Combine the root directory with the script directory
+    $startingDirectory = Join-Path $PSScriptRoot $functionDirectory
     
     # Set the working directory to the starting directory
     $workingDirectory = $startingDirectory
     Set-Location $workingDirectory
 
+    $logoForMenu =  @"
+     _   _   _   _   _   _   _     _   _   _   _   _   _   _            
+    / \ / \ / \ / \ / \ / \ / \ | / \ / \ / \ / \ / \ / \ / \ 
+   ( C | o | m | m | a | n | d  |  C | e | n | t | r | a | l )
+    \_/ \_/ \_/ \_/ \_/ \_/ \_/ | \_/ \_/ \_/ \_/ \_/ \_/ \_/                                                             
+                                         By Admiral-AI
+*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+         Making the simple complicated is commonplace;            
+Making the complicated simple, awesomely simple, that's creativity
+
+"@
+
     # Loop until the user chooses to quit
     while ($userChoice -ne 'quit') {
+
+        Write-Host $logoForMenu -ForegroundColor DarkYellow
+
         # Check if the starting and working directories match
         if ($startingDirectory -eq $workingDirectory) {
             $optionToQuit = 1
@@ -69,7 +135,7 @@ function Set-DisplayMenu {
         }
 
         # Get user input
-        $userChoice = Read-Host "Enter the number corresponding to your choice:"
+        $userChoice = Read-Host "Enter the number corresponding to your choice"
         
         # Process user choice
         if ($userChoice -le $ps1Options.Count) {
@@ -109,6 +175,7 @@ function Set-DisplayMenu {
  
     # Clear the console
     Clear-Host
+
 }
 
 # Call the Main function
