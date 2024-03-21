6b30a9639970612c1f392b843126470271ad84c3
Admiral-AI <commandcentralgithub@gmail.com>
2024-03-21 16:35:39 -0400

    v1.0 (#2)
    Added credential validation instead of blindly accepting credentials, also set up update locations and settings file
    Slimmed down code, remove nested lists inside arrays inside etc etc etc
    
    Tuned to meet production standards

--- CommandCentral.ps1
+++ CommandCentral.ps1
@@ -1,33 +1,54 @@
+# Junk?:
+<#
+param (
+    [string]$startingDirectory
+)
+#>
+
+#Test updates based on this line
+
 # Main function
 function Main {
-    
-    # testing Updates using this line
+
+    Set-Location $PSScriptRoot
+
+    # Import settings file and set the variable to global access (indicates that all scripts within the current PowerShell session can access it)
+    Set-Variable -Name settingsJSON -Scope global
+    $settingsJSON = Get-Content .\SystemD\Settings.json
+    $settingsJSON = $settingsJSON | ConvertFrom-Json
 
     # Set transcript log path based on 
-    $tramscriptLogPath = Join-Path -Path $PSScriptRoot -ChildPath "\SystemD\Log\transcriptLog.txt"
-    Start-Transcript $tramscriptLogPath
-
-    # Set the github repository location
-    $scriptGithubUri = "https://github.com/Admiral-AI/CommandCentral/raw/main/CommandCentral.ps1"
+    Start-Transcript $settingsJSON.Application_Settings.Log_Paths.CommandCentral_PSScript_Log -Append -Force
 
     # Clear the console
     Clear-Host
 
-    Get-UserCredentials
+    # Start the domino effect of functions based on setting file update parameter
+    if (($settingsJSON.Application_Settings.Updates.UpdatesOptIn) -eq $true) {
+        Get-Updates
+    } elseif (($settingsJSON.Application_Settings.Updates.UpdatesOptIn) -eq $false) {
+        Get-UserCredentials
+    } else {
+        Write-Host "Setting file is unreadable or missing the update opt in parameter, please re-download or fix your setting file"
+    }
 
+    # Reset location before exiting, prevents errors when runnning in same terminal
+    Set-Location $PSScriptRoot
+
+    # Stop Transcript when finished with script
     Stop-Transcript
 }
 
 # Function to get user credentials
 function Get-UserCredentials {
-    <#
-    param (
-        [string]$startingDirectory
-    )
-    #>
+
+    # Set the userCredArray variable to global to allow all scripts that are run within this powershell session to access the credentials
+    Set-Variable -Name userCredArray -Scope global
+    $userCredArray = @()
     
     # Get the CIM_ComputerSystem CIM class
     $computerSystem = Get-CimInstance Win32_ComputerSystem
+
     # Check if the domain property is not empty
     if ($computerSystem.PartofDomain -eq $true) {
         Write-Host "Computer is in a domain: $($computerSystem.Domain)"
@@ -40,43 +61,130 @@ function Get-UserCredentials {
             $userCred = Get-StoredCredential -Target "CommandCentral-$($accountToQuery.SamAccountName)"
             
             if ($null -eq $userCred) {
-                 $userCred = Get-Credential -UserName "$($Env:UserDomain)\$($accountToQuery.SamAccountName)" -Message "Please provide your credentials for the following account: $($Env:UserDomain)\$($accountToQuery.SamAccountName)"
-                 New-StoredCredential -Target "CommandCentral-$($accountToQuery.SamAccountName)" -Credential $userCred -Persist ENTERPRISE
-            }
-        }
 
+                # Set loop counter and exit values
+                $loopCounter_userCredCheck = 0
+                $loopControl_userCredCheck = $true
+                
+                # Begin loop to check credential validity
+                while ($loopControl_userCredCheck) {
+                    
+                    if ($loopCounter_userCredCheck -ge 3) {
+                        Write-Warning -Message ("Take a deep breath and perhaps a break. You have entered your password $($loopCounter_userCredCheck) times incorrectly")
+                        Write-Host "We will now take a 30 second break to slow down"
+                        Start-Sleep -Seconds 30
+                    }
+            
+                    # Collect the username and password and store in credential object.
+                    $userCred = Get-Credential -UserName "$($Env:UserDomain)\$($accountToQuery.SamAccountName)" -Message "Please provide your credentials for the following account: $($Env:UserDomain)\$($accountToQuery.SamAccountName)"
+
+                    try {
+                        # Build the current domain
+                        $currentDomain = "LDAP://$($userCred.GetNetworkCredential().Domain)"
+            
+                        # Get the user\password. The GetNetworkCredential only works for the passwrod because the current user
+                        # is the one who entered it.  Shouldn't be accessible to anything\one else.
+                        $userName = $userCred.GetNetworkCredential().UserName
+                        $password = $userCred.GetNetworkCredential().Password
+                    } catch {
+                        Write-Warning -Message ("There was a problem with what you entered: $($_.exception.message)")
+                        continue
+                    }
+            
+                    # Do a quick query against the domain to authenticate the user.
+                    $domainQuery = New-Object System.DirectoryServices.DirectoryEntry($currentDomain,$userName,$password)
+                    # If we get a result back with a name property then we're good to go and we can store the credential.
+                    if ($domainQuery.name) {
+                        Write-Host "Credential for: $($userCred.Username) was succesfully validated! Adding to storage..."
+                        $userCred = New-StoredCredential -Target "CommandCentral-$($accountToQuery.SamAccountName)" -Credential $userCred -Persist ENTERPRISE
+                        $userCred = Get-StoredCredential -Target "CommandCentral-$($accountToQuery.SamAccountName)"
+                        $userCredArray += $userCred
+
+                        $loopControl_userCredCheck = $false
+                        Remove-Variable password -Force
+                    } else {
+                        $loopCounter_userCredCheck++
+                        Write-Warning -Message ("The password you entered for $($userName) was incorrect.  Attempt(s) $($loopCounter_userCredCheck). Please try again.")
+                    }
+                
+                }
+            } elseif ($userCred) {
+
+                $userCredArray += $userCred
+
+            } else {
+
+                Write-Host "Something happened...try restarting the script, redownload, or contact you administrator"
+                Start-Sleep 5
+
+            }
+
+            # Clear variable to ensure it can't be misused or run into errors
+            Clear-Variable -Name userCred -Force
+
+        }
     } else {
+
         Write-Host "Computer is in a workgroup, credentials are assumed to not be needed (administrator running script)"
-        Sleep 2
+        Start-Sleep 2
+
     }
+
+    Write-Host "Finished credential validation, proceeding to menu..."
 
     # Clear the screen before heading to the next functions
     Clear-Host
 
-    # Call the Check-Updates function
-    Check-Updates
+    # Call the menu function
+    Set-DisplayMenu
 
 }
 
 # Function to check for module and script updates
-function Check-Updates {
+function Get-Updates {
     
     #Need to add check for RSAT install
-    <#if($null -eq (Get-Module -ListAvailable -Name ActiveDirectory))
-    {
-        Write-Host -ForegroundColor Red "Remote Server Administration Tools isn't installed!"
-        Try{
-            Start-Process powershell -Verb runas -PassThru -ArgumentList `
-            "'Installing RSAT...'; Get-WindowsCapability -Name RSAT.ActiveDirectory* -Online | Add-WindowsCapability -Online; Read-Host 'Please reboot your computer to complete installation'"
-            }
-        Catch{Write-Host -ForegroundColor Red "Installation failed. Please install manually and run this tool again";exit}
-    }#>
+    <#
+    # Get the CIM_ComputerSystem CIM class and set variable to global
+    $computerSystem = Get-CimInstance Win32_ComputerSystem
 
-    $scriptOnGithub = $(Invoke-RestMethod -Uri $scriptGithubUri)
+    # Check if the domain property is not empty
+    if ($computerSystem.PartofDomain -eq $true) {
+        if($null -eq (Get-Module -ListAvailable -Name ActiveDirectory))
+        {
+            Write-Host -ForegroundColor Red "Remote Server Administration Tools isn't installed!"
+            Try{
+                Start-Process powershell -Verb runas -PassThru -ArgumentList `
+                "'Installing RSAT...'; Get-WindowsCapability -Name RSAT.ActiveDirectory* -Online | Add-WindowsCapability -Online; Read-Host 'Please reboot your computer to complete installation'"
+                }
+            Catch{Write-Host -ForegroundColor Red "Installation failed. Please install manually and run this tool again";exit}
+        }
+    }
+    #>
+
+    $providedJSONUpdateLocation = $settingsJSON.Application_Settings.Updates.UpdateRetrievalLocation
+
+    switch ($providedJSONUpdateLocation) {
+        "Main_Repo" {$repoToPullFrom = $settingsJSON.Application_Settings.Updates.Main_Repo.CommandCentral_Script_mainGitRepo ; break}
+        "Dev_Repo" {$repoToPullFrom = $settingsJSON.Application_Settings.Updates.Dev_Repo.CommandCentral_Script_devGitRepo ; break}
+        "Local_Repo" {$repoToPullFrom = $settingsJSON.Application_Settings.Updates.Local_Repo.CommandCentral_Script_localRepo ; break}
+    }
+
+    if ((Invoke-WebRequest $repoToPullFrom -DisableKeepAlive -UseBasicParsing -Method Head).StatusDescription -eq "OK") {
+        $scriptPulledfromRepo = $(Invoke-RestMethod -Uri $repoToPullFrom)
+    } elseif ((Test-Path $repoToPullFrom) -eq $true) {
+        $scriptPulledfromRepo = Get-Content -Path $($repoToPullFrom) -Raw
+    } else {
+        Write-Host "General failure when pulling information from repository, please re-run script again or redownload the files."
+        if ($repoToPullFrom -eq "Local_Repo") {
+            Write-Host "Alternatively contact your administrator since this is a locally run repository"
+        }
+    }
+
     $scriptOnLocalDisk = Get-Content -Path $($PSCommandPath) -Raw
 
     # Use Compare-Object to compare the contents of the two files
-    $fileComparison = Compare-Object -ReferenceObject ($scriptOnGithub) -DifferenceObject ($scriptOnLocalDisk)
+    $fileComparison = Compare-Object -ReferenceObject ($scriptPulledfromRepo) -DifferenceObject ($scriptOnLocalDisk)
 
     # Check the result
     if ($fileComparison.Count -eq 0) {
@@ -94,81 +202,72 @@ function Check-Updates {
 
         foreach ($module in $moduleList) {
             if (Get-Module -ListAvailable -Name $module) {
-                Write-Host "Module: $($module) exists"
+                Write-Host "Module: $($module) exists, no need to import or install"
             } 
             else {
                 Write-Host "Module: $($module) does not exist. Installing..."
                 Install-Module -Name $module -Scope CurrentUser -Force
+                Write-Host "Importing $($module) module..."
+                Import-Module -Name $module -Force
             }
         }
         
         # Clear the screen before heading to the next functions
         Clear-Host
         
-        # Call the Set-DisplayMenu function
-        Set-DisplayMenu
+        # Call the Get-UserCredentials function
+        Get-UserCredentials
+
     } else {
-        Invoke-WebRequest -Uri $scriptGithubUri -OutFile $PSCommandPath
-        Write-Host "Attempted to pull update, now opening script again..."
+        
+        $scriptPulledfromRepo | Out-File -FilePath $($PSCommandPath) -NoNewline
+
+        Write-Host "Attempted to write updates, now opening script again..."
         Start-Sleep 2
         Start-Process powershell.exe -ArgumentList "-File `"$($PSCommandPath)`""
         Write-Host "Exiting CommandCentral..."
         Start-Sleep .75
+
     }
 }
 
 # Function to display a menu and handle user input
 function Set-DisplayMenu {
-    <# Parameters from Main function
-    param (
-        [string]$startingDirectory
-    )
-    #>
 
-    # Define the directory where the PowerShell scripts and 'menus' are located
-    $functionDirectory = "Functions"
-    # Combine the root directory with the script directory
-    $startingDirectory = Join-Path $PSScriptRoot $functionDirectory
-    
-    # Set the working directory to the starting directory
-    $workingDirectory = $startingDirectory
-    Set-Location $workingDirectory
+    # Define the directories where the PowerShell scripts, 'menus', and settings are located
+    $rootScriptDirectory = $PSScriptroot
+    $systemDDirectory = Join-Path -Path $PSScriptroot -ChildPath $settingsJSON.Application_Settings.SystemD_Path
+    $functionsDirectory = Join-Path -Path $rootScriptDirectory -ChildPath $settingsJSON.Application_Settings.Functions_Path
 
-    $logoForMenu =  @"
-      _   _   _   _   _   _   _     _   _   _   _   _   _   _            
-     / \ / \ / \ / \ / \ / \ / \ | / \ / \ / \ / \ / \ / \ / \ 
-    ( C | o | m | m | a | n | d  |  C | e | n | t | r | a | l )
-     \_/ \_/ \_/ \_/ \_/ \_/ \_/ | \_/ \_/ \_/ \_/ \_/ \_/ \_/                                                             
-                                          By Admiral-AI
-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-           Making the simple complicated is commonplace;            
-Making the complicated simple, awesomely simple, that's creativity!
+    # Set the starting directory to the location of the 'Functions' directory
+    $startingDirectory = $functionsDirectory
+    Set-Location $functionsDirectory
 
-"@
+    # Using SystemD location setting, pull logo file
+    $logoFileLocation = Join-Path -Path $($systemDDirectory) -ChildPath \Logo.txt
+    $logoForMenu = Get-Content $logoFileLocation -Raw
 
-    # Loop until the user chooses to quit
+    # Loop to display menus until the user chooses to quit
     while ($userChoice -ne 'quit') {
 
         Write-Host $logoForMenu -ForegroundColor DarkYellow
 
-        # Check if the starting and working directories match
-        if ($startingDirectory -eq $workingDirectory) {
+        # Check if the starting and working directories match and provide option to quit accordingly
+        if ($startingDirectory -eq $functionsDirectory) {
             $optionToQuit = 1
-            # Debug: Provide information about the option to quit
         } else {
             $optionToQuit = 0
-            # Debug: Provide information about the option to go back one directory
         }
 
         # List .ps1 files in the working directory
-        $ps1FilesArray = Get-ChildItem -Path $workingDirectory -Filter "*.ps1"
+        $ps1FilesArray = Get-ChildItem -Path $functionsDirectory -Filter "*.ps1"
         $ps1Options = @()
         Foreach ($ps1File in $ps1FilesArray) {
             $ps1Options += ($ps1File.BaseName)
         }
 
         # List subdirectories in the working directory
-        $subdirectories = Get-ChildItem -Path $workingDirectory -Directory
+        $subdirectories = Get-ChildItem -Path $functionsDirectory -Directory
         $subOptions = @()
         Foreach ($subDirectory in $subdirectories) { 
             $subOptions += ($subDirectory.BaseName)
@@ -199,7 +298,7 @@ Making the complicated simple, awesomely simple, that's creativity!
         if ($userChoice -le $ps1Options.Count) {
             # Run the selected PowerShell script
             $scriptSelected = $ps1Options[$userChoice - 1] + ".ps1"
-            $scriptPath = Join-Path $workingDirectory $scriptSelected #.FullName
+            $scriptPath = Join-Path $functionsDirectory $scriptSelected #.FullName
             Write-Host "Running script: $($ps1Options[$userChoice - 1])" -ForegroundColor Green
             Start-Sleep .75
             Unblock-File $scriptPath
@@ -212,13 +311,13 @@ Making the complicated simple, awesomely simple, that's creativity!
                 Write-Host "Selected subdirectory is empty." -ForegroundColor Red
                 Start-Sleep .75
             } else {
-                $workingDirectory = Join-Path $workingDirectory $selectedDir
+                $functionsDirectory = Join-Path $functionsDirectory $selectedDir
                 Write-Host "Entered the selected subdirectory." -ForegroundColor Blue
                 Start-Sleep .75
             }
         } elseif (($userChoice -eq $($($ps1Options.Count) + $($subOptions.Count) + 1) -and ($optionToQuit -eq 0))) {
             # Go back one directory if not in the starting directory
-            $workingDirectory = Split-Path -Path $workingDirectory -Parent
+            $functionsDirectory = Split-Path -Path $functionsDirectory -Parent
             Clear-Host
             Write-Host "Went back one directory." -ForegroundColor Cyan
         } elseif ((($userChoice -eq $($($ps1Options.Count) + $($subOptions.Count) + 1) -or ($userChoice -eq 'q') -or ($userChoice -eq 'quit'))-and ($optionToQuit -eq 1))) {
--- README.md
+++ README.md
@@ -1,64 +1,30 @@
-# Command Central (Under Development - 1 Function Released, 1 Under Construction)
+# Command Central
+CommandCentral is a versatile PowerShell tool designed to streamline various administrative tasks with menu-driven navigation and built-in credential stores. This README provides an overview of the tool's functionality and how to use it effectively.
 
-## Introduction
+## <------ Table of Contents ------>
+**IN PROGRESS, READ ON**
+## <------  End of Contents  ------>
 
-Command Central is a powerful GUI tool under construction that is designed for Helpdesk and Desktop Support Technicians (for advanced profesionals too!). It simplifies common tasks and automates various processes, making it an invaluable resource for both beginners and advanced professionals in the IT field.
+**Features:**
+1. **Menu-Driven Navigation:** CommandCentral provides a user-friendly menu interface for navigating through PowerShell scripts and directories. Users can easily select scripts to run, explore subdirectories, or navigate back to previous directories. This intuitive interface enhances usability and efficiency when managing multiple scripts and directories.
+2. **Script Updates:** The tool includes functionality to check for updates to the main script stored on a GitHub repository. It compares the local script with the version on GitHub and automatically updates if discrepancies are found. This ensures users always have access to the latest version of the tool.
+3. **Credential Management:** CommandCentral simplifies the process of managing user credentials, particularly in domain environments. It automatically detects whether the computer is part of a domain and prompts users to provide their credentials if necessary. It also supports storing and retrieving credentials securely using PowerShell's Credential Manager.
 
-## Features
+**Usage:**
+1. **Getting Started:** Clone or download the CommandCentral repository from GitHub to your local machine and extract if neccesary (It is preferable to save it to "C:\Users\%USERNAME%\Appdata\Roaming" for cleanliness and has the added benefit of being able to roam with the script accross multiple computers in a domain if your profile is configured to do so.
+3. **Running the Script:** Create a shortcut of the `CommandCentral.ps1` on your desktop, right click on it, then hit "Run with powershell" or open PowerShell and navigate to the directory where CommandCentral is stored. Run the `CommandCentral.ps1` script to launch the tool.
+4. **Credential Management:** If the computer is part of a domain, CommandCentral will prompt you to provide your credentials. These credentials are securely stored via Microsoft's DPAPI algorithm and can be retrieved automatically for subsequent use.
+5. **Checking for Updates:** CommandCentral automatically checks for updates to the main script on GitHub. If updates are available, it will prompt you to update the local script to the latest version.
+6. **Navigating Menus:** Use the displayed menu options to navigate through scripts and directories. Select the desired option by entering the corresponding number or quit the tool by entering 'q' or 'quit'.
 
-- **Uninstallation Automation:** Command Central can help you identify and uninstall unwanted software from your system, streamlining the cleanup process.
-
-- **App Approval System:** The script includes an app approval system that ensures critical applications remain untouched, reducing the risk of accidental removal.
-
-- **Uninstall Options Database:** Command Central maintains a database of uninstallation options for different applications, providing more control over the process.
-
-- **Silent Uninstallations:** The tool supports silent uninstallations using common switches for MSI-installed and other types of applications.
-
-## Usage
-
-### Prerequisites
-
-Before using Command Central, ensure that you have the following prerequisites in place:
-
-- PowerShell 5.1 or higher
-- Administrator privileges (required for uninstallations)
-
-### Installation
-
-1. Clone or download the Command Central repository from GitHub.
-2. Navigate to the project directory.
-
-### Running Command Central
-
-To run Command Central:
-
-1. Open a PowerShell window with administrative privileges.
-2. Navigate to the Command Central directory.
-3. Execute the script by running the `UninstallUnapprovedAppsV0.01.ps1` file.
-
-### Configuration
-
-- Customize the list of pre-approved apps in the `Start_Variables` function.
-- Maintain your uninstall options database by modifying the `uninstallOptionsDB.csv` file.
-
-## Known Issues
-
-- No known issues at the moment. Please report any problems in the [Issues](https://github.com/MissionControlFreak/Command Central/issues) section.
-
-## Contributing
-
-If you'd like to contribute to Command Central, please follow our [Contributing Guidelines](CONTRIBUTING.md).
-
-## License
-
+**Contributing:**
+Contributions to CommandCentral are welcome! If you encounter any issues, have suggestions for improvements, or would like to contribute new features, please open an issue or submit a pull request on GitHub.
+**License:**
 This project is licensed under the GNU GENERAL PUBLIC License v3.0 - see the [LICENSE](LICENSE) file for details.
 
-## Acknowledgments
+**Acknowledgments:**
+CommandCentral was developed by [Admiral-AI](https://github.com/Admiral-AI) to simplify administrative tasks and enhance PowerShell scripting capabilities. I extend my gratitude to the entire PowerShell community for their support and contributions.
 
-Command Central was developed with the support of the open-source community and various libraries and resources.
-
-## Contact
-
----
-
-Enjoy using Command Central and simplify your IT support tasks!
+**Contact:**
+For questions, feedback, or support inquiries, please contact [Admiral-AI](https://github.com/Admiral-AI) via GitHub.
+Thank you for using CommandCentral! We hope it helps streamline your administrative tasks and improves your PowerShell scripting experience.
