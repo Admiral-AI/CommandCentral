af9c1d9f709ebae937b9f17fdcf61442121ec256
Admiral-AI <commandcentralgithub@gmail.com>
2024-06-06 09:12:38 -0400

    Revised CommandCentral.ps1
    Removed credential function, going to make a password store script that will be run inside the menu as an option to provide flexibility with saved credentials

--- CommandCentral.ps1
+++ CommandCentral.ps1
@@ -16,8 +16,7 @@ function Main {
 
     # Import settings file and set the variable to script access (indicates that all scripts run from this one can access the variable when passed)
     Set-Variable -Name settingsJSON -Scope script
-    $settingsJSON = Get-Content .\SystemD\Settings.json
-    $settingsJSON = $settingsJSON | ConvertFrom-Json
+    $settingsJSON = Get-Content .\SystemD\Settings.json | ConvertFrom-Json
 
     # Set transcript log path based on 
     Start-Transcript $settingsJSON.Application_Settings.Log_Paths.CommandCentral_PSScript_Log -Append -Force
@@ -29,220 +28,20 @@ function Main {
     if (($settingsJSON.Application_Settings.Updates.UpdatesOptIn) -eq $true) {
         Get-Updates
     } elseif (($settingsJSON.Application_Settings.Updates.UpdatesOptIn) -eq $false) {
-        Get-UserCredentials
+        Set-DisplayMenu
     } else {
-        Write-Host "Setting file is unreadable or missing the update opt in parameter, please re-download or fix your setting file"
+        Write-Host "Settings file is unreadable or is missing the update opt in parameter, please re-download the script or fix your setting file"
     }
 
-    # Reset location before exiting, prevents errors when runnning in same terminal
+    # Reset location before exiting, prevents errors and allows you to re-run the script in same terminal window
     Set-Location $PSScriptRoot
 
     # Stop Transcript when finished with script
     Stop-Transcript
 }
 
-# Function to get user credentials
-function Get-UserCredentials {
-
-    # Load nested functions
-    function Prompt_UserCredentials {
-        param (
-            [Parameter(Mandatory=$true)] [Object[]] $accountToQuery
-        )
-
-        $loopControl_UserCredentialPrompt = $true
-
-        while ($loopControl_UserCredentialPrompt -eq $true) {
-            # Collect the username and password and store in credential object.
-            $userCred = Get-Credential -UserName "$($Env:UserDomain)\$($accountToQuery.SamAccountName)" -Message "Please provide your credentials for the following account: $($Env:UserDomain)\$($accountToQuery.SamAccountName)"
-
-            if ($null -ne $userCred) {
-                $nullCredentialTest = (($null -ne ($userCred.GetNetworkCredential().Password)) -and (($userCred.GetNetworkCredential().Password) -ne ""))
-            }
-
-            if ($nullCredentialTest -eq $true) {
-                $loopControl_UserCredentialPrompt = $false
-            } else {
-                Write-Host "The supplied credntial was empty, please input a value"
-            }
-
-        }
-
-        return $userCred
-    }
-
-    function Validate_UserCredentials {
-        param (
-            [Parameter(Mandatory=$true)] [pscredential] $userCred,
-            [Parameter(Mandatory=$true)] [Object[]] $accountToQuery,
-            [Parameter(Mandatory=$true)] [AllowEmptyCollection()] [hashtable] $userCreds_HashTable
-        )
-
-        try {
-            # Build the current domain
-            $currentDomain = "LDAP://$($userCred.GetNetworkCredential().Domain)"
-
-            # Get the user\password. The GetNetworkCredential only works for the passwrod because the current user
-            # is the one who entered it.  Shouldn't be accessible to anything\one else.
-            $username = $userCred.GetNetworkCredential().Username
-            $password = $userCred.GetNetworkCredential().Password
-        } catch {
-            Write-Warning -Message ("There was a problem with what you entered: $($_.exception.message)")
-            continue
-        }
-
-        # Do a quick query against the domain to authenticate the user.
-        $domainQuery = New-Object System.DirectoryServices.DirectoryEntry($currentDomain,$username,$password)
-        # If we get a result back with a name property then we're good to go and we can store the credential.
-        if ($domainQuery.name) {
-            Write-Host "Credential for: $($userCred.Username) was succesfully validated! Adding to storage..."
-            New-StoredCredential -Target "CommandCentral-$($accountToQuery.SamAccountName)" -Credential $userCred -Persist ENTERPRISE
-            $userCred = Get-StoredCredential -Target "CommandCentral-$($accountToQuery.SamAccountName)"
-            $userCreds_HashTable[$userCred.UserName] = $userCred
-
-            $loopControl_userCredCheck = $false
-            $loopCounter_userCredCheck = 0
-            Remove-Variable password -Force
-        } else {
-            $loopCounter_userCredCheck++
-            Write-Warning -Message ("The password you entered for $($username) was incorrect.  Attempt(s) $($loopCounter_userCredCheck). Please try again.")
-        }
-
-        $validationFunction_ReturnHashtable = @{
-            UserCreds_HashTable = $userCreds_HashTable
-            LoopCounter_UserCredCheck = $loopCounter_userCredCheck
-            LoopControl_UserCredCheck = $loopControl_userCredCheck
-        }
-
-        return $validationFunction_ReturnHashtable
-
-    }
-
-    # Set the UserCreds_HashTable variable to script to allow all scripts that are run from this script to access the credentials when passed
-    Set-Variable -Name UserCreds_HashTable -Scope script
-    $userCreds_HashTable = @{}
-
-    # Get the CIM_ComputerSystem CIM class
-    $computerSystem = Get-CimInstance Win32_ComputerSystem
-
-    # Check if the domain property is not empty
-    if ($computerSystem.PartofDomain -eq $true) {
-        Write-Host "Computer is in a domain: $($computerSystem.Domain)"
-        
-        $userFirstName = $(Get-ADUser -Identity $env:username).GivenName
-        $userLastName = $(Get-ADUser -Identity $env:username).Surname
-        $accountsToQuery = Get-ADUser -Filter $("(GivenName -like '*$($userFirstName)*') -and (sn -like '*$($userLastName)*') -and (Enabled -eq 'True')")
-
-        foreach ($accountToQuery in $accountsToQuery) {
-
-            # Set the loop control starting values
-            $loopCounter_userCredCheck = 0
-            $loopControl_userCredCheck = $true
-
-            try {
-                $userCred = Get-StoredCredential -Target "CommandCentral-$($accountToQuery.SamAccountName)"
-            } catch {
-                Write-Host "Retrieving stored credential failed, check the runtime log, re-install/run the script or contact your administrator"
-                Write-Host "Proper error handling was not added here, exiting script"
-                break
-            }
-
-            if ($null -eq $userCred) {
-
-                while ($loopControl_userCredCheck -eq $true) {
-
-                    if ($loopCounter_userCredCheck -ge 3) {
-                        Write-Warning -Message ("Take a deep breath and perhaps a break. You have entered your password $($loopCounter_userCredCheck) times incorrectly")
-                        Write-Host "We will now take a 30 second break to slow down"
-                        Start-Sleep -Seconds 30
-                    }
-
-                    $userCred = Prompt_UserCredentials -accountToQuery $accountToQuery
-
-                    $validationFunction_ReturnHashtable = Validate_UserCredentials -userCred $userCred -accountToQuery $accountToQuery -UserCreds_HashTable $userCreds_HashTable
-
-                    $loopCounter_userCredCheck = $validationFunction_ReturnHashtable.LoopCounter_UserCredCheck
-                    $loopControl_userCredCheck = $validationFunction_ReturnHashtable.LoopControl_UserCredCheck
-                    $userCreds_HashTable = $validationFunction_ReturnHashtable.UserCreds_HashTable
-                    
-                }
-
-            } elseif ($null -ne $userCred) {
-
-                # One time credential check since credentials are stored, if they are good then the while block should be skipped
-                $validationFunction_ReturnHashtable = Validate_UserCredentials -userCred $userCred -accountToQuery $accountToQuery -UserCreds_HashTable $userCreds_HashTable
-
-                $loopCounter_userCredCheck = $validationFunction_ReturnHashtable.LoopCounter_UserCredCheck
-                $loopControl_userCredCheck = $validationFunction_ReturnHashtable.LoopControl_UserCredCheck
-                $userCreds_HashTable = $validationFunction_ReturnHashtable.UserCreds_HashTable
-
-                while ($loopControl_userCredCheck -eq $true) {
-
-                    if ($loopCounter_userCredCheck -ge 3) {
-                        Write-Warning -Message ("Take a deep breath and perhaps a break. You have entered your password $($loopCounter_userCredCheck) times incorrectly")
-                        Write-Host "We will now take a 30 second break to slow down"
-                        Start-Sleep -Seconds 30
-                    }
-
-                    $userCred = Prompt_UserCredentials -accountToQuery $accountToQuery
-
-                    $validationFunction_ReturnHashtable = Validate_UserCredentials -userCred $userCred -accountToQuery $accountToQuery -UserCreds_HashTable $userCreds_HashTable
-
-                    $loopCounter_userCredCheck = $validationFunction_ReturnHashtable.LoopCounter_UserCredCheck
-                    $loopControl_userCredCheck = $validationFunction_ReturnHashtable.LoopControl_UserCredCheck
-                    $userCreds_HashTable = $validationFunction_ReturnHashtable.UserCreds_HashTable
-                    
-                }
-
-            } else {
-
-                Write-Host "Something happened...try restarting the script, redownload, or contact your administrator"
-                Start-Sleep 5
-
-            }
-
-            # Clear variable to ensure it can't be misused or run into errors
-            Clear-Variable -Name userCred -Force
-
-        }
-    } else {
-
-        Write-Host "Computer is in a workgroup, credentials are assumed to not be needed (administrator running script)"
-        Start-Sleep 2
-
-    }
-
-    Write-Host "Finished credential validation, proceeding to menu..."
-
-    # Clear the screen before heading to the next functions
-    Clear-Host
-
-    # Call the menu function
-    Set-DisplayMenu
-
-}
-
 # Function to check for module and script updates
 function Get-Updates {
-
-    <# Need to add check for RSAT install
-    # Get the CIM_ComputerSystem CIM class and set variable to global
-    $computerSystem = Get-CimInstance Win32_ComputerSystem
-
-     Check if the domain property is not empty
-    if ($computerSystem.PartofDomain -eq $true) {
-        if($null -eq (Get-Module -ListAvailable -Name ActiveDirectory))
-        {
-            Write-Host -ForegroundColor Red "Remote Server Administration Tools isn't installed!"
-            Try{
-                Start-Process powershell -Verb runas -PassThru -ArgumentList `
-                "'Installing RSAT...'; Get-WindowsCapability -Name RSAT.ActiveDirectory* -Online | Add-WindowsCapability -Online; Read-Host 'Please reboot your computer to complete installation'"
-                }
-            Catch{Write-Host -ForegroundColor Red "Installation failed. Please install manually and run this tool again";exit}
-        }
-    }
-    #>
-    
 
     $providedJSONUpdateLocation = $settingsJSON.Application_Settings.Updates.UpdateRetrievalLocation
 
@@ -300,8 +99,7 @@ function Get-Updates {
         # Call the Get-UserCredentials function
         Get-UserCredentials
 
-    } else {
-        
+    } else { 
         $scriptPulledfromRepo | Out-File -FilePath $($PSCommandPath) -NoNewline
 
         Write-Host "Attempted to write updates, now opening script again..."
@@ -309,7 +107,6 @@ function Get-Updates {
         Start-Process powershell.exe -ArgumentList "-File `"$($PSCommandPath)`""
         Write-Host "Exiting CommandCentral..."
         Start-Sleep .75
-
     }
 }
 
@@ -362,7 +159,6 @@ function Set-DisplayMenu {
             $subOptions += ($subDirectory.BaseName)
         }
 
-
         # Display options to the user
         Write-Host "Options:"
         # Display .ps1 files in green
@@ -390,7 +186,13 @@ function Set-DisplayMenu {
             $scriptPath = Join-Path $workingDirectory $scriptSelected #.FullName
             Write-Host "Running script: $($ps1Options[$userChoice - 1])" -ForegroundColor Green
             Start-Sleep .75
-            Unblock-File $scriptPath
+
+            # Unblock the file on Windows as windows will block downloaded scripts.
+            # Any PowerShell version less than 6 is Windows as it was not cross platform then. 
+            # Any version above 6 will have the "IsWindows" variable.
+            if (($PSVersionTable.PSVersion.Major -lt 6) -or ($IsWindows -eq $true)) {
+                Unblock-File $scriptPath
+            }
             . $scriptPath -CC_MainMenu_HashTable $CC_MainMenu_HashTable
             
         } elseif ($userChoice -le ($ps1Options.Count + $subOptions.Count)) {
@@ -418,16 +220,11 @@ function Set-DisplayMenu {
             Write-Host "Invalid entry, please enter an option from the list" -ForegroundColor Red
             Start-Sleep .75
         }
-        
         # Clear the console
         Clear-Host
-
     }
- 
     # Clear the console
     Clear-Host
-
 }
-
 # Call the Main function
 Main
